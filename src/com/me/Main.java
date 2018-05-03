a0f997360e8370e3bfede05230b1c4b9503a8787
patrickatcw <pj8392@yahoo.com>
2018-05-03 12:31:00 -0400

    created and ran 3 new instances of class, results commented

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,4 +1,19 @@
 package com.me;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //step 5 instance
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+        //then run
+        //results;
+        /*one created, instance is 1
+          two created, instance is 2
+          three created, instance is 3*/
+
+    }
+
 }
